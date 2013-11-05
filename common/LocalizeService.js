46b1be223de907a973cb65176b9c167e8fe1ba01
ryancanulla <ryancanulla@gmail.com>
2013-11-04 22:54:21 -0500

    add localization

---
+++ common/LocalizeService.js
@@ -0,0 +1,26 @@
+Ext.define('SampleCommonApp.LocalizeService', {
+    mixins: { observable: 'Ext.util.Observable' },
+    localizedData: {},
+
+    requires: ['Ext.Ajax'],
+
+    constructor: function (config) {
+        this.mixins.observable.constructor.call(this, config);
+        this.loadLocalizedHash();
+    },
+
+    loadLocalizedHash:function() {
+        Ext.Ajax.request({
+            url: 'resources/i18n/en-US.json',
+            success: function(response){
+                this.localizedData = Ext.JSON.decode(response.responseText);
+                this.fireEvent('loaded');
+            },
+            scope: this
+        });
+    },
+
+    map: function(key) {
+        return this.localizedData[key];
+    }
+});
